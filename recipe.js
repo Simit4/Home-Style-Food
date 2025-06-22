08e0a27add7b3d6a0797f54faf7603fda5ed57b4
Simit4 <simitdhamala@gmail.com>
2025-06-23 08:48:24 +1000

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://ozdwocrbrojtyogolqxn.supabase.co'; // your Supabase project URL
-const SUPABASE_ANON_KEY = 'your-anon-key-here'; // replace with your anon key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZHdvY3Jicm9qdHlvZ29scXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NzE5MzMsImV4cCI6MjA2NjE0NzkzM30.-MAiUtrdza-T2q8POxY-ZcZuZr5QYzFYq5yd-bVYzRQ'; // replace with your anon key
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
