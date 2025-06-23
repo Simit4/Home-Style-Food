091b75b2459d3910f47cc0f8c4365554850b5ed8
Simit4 <simitdhamala@gmail.com>
2025-06-23 20:40:07 +1000

    Update equipment.js

--- equipment.js
+++ equipment.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Supabase config
 const SUPABASE_URL = 'https://ozdwocrbrojtyogolqxn.supabase.co'; // Replace with your Supabase URL
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE'; // Replace with your Supabase anon key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZHdvY3Jicm9qdHlvZ29scXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NzE5MzMsImV4cCI6MjA2NjE0NzkzM30.-MAiUtrdza-T2q8POxY-ZcZuZr5QYzFYq5yd-bVYzRQ'; // Replace with your Supabase anon key
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
